66161d9c0198381ae0d11d94b3f1fdf720d1db15
Chris Troutner <chris.troutner@gmail.com>
2016-08-03 20:13:19 -0700

    removing dotenv as it doesn't seem to be working.

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 'use strict';
 
-// Simulate config options from your production environment by
-// customising the .env file in your project's root folder.
-require('dotenv').load();
-
-
 /*
  * Express Dependencies
  */
@@ -52,68 +47,24 @@ keystone.import('models');
 // Serve your static assets
 app.use(serve('./assets'));
 
-/*
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
 
-var Cat = mongoose.model('Cat', { name: String });
+app.engine('handlebars', exphbs({
+    // Default Layout and locate layouts and partials
+    defaultLayout: 'main',
+    layoutsDir: 'views/layouts/',
+    partialsDir: 'views/partials/'
+}));
 
-var kitty = new Cat({ name: 'Zildjian' });
-kitty.save(function (err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('meow');
-  }
-});
+// Locate the views
+app.set('views', __dirname + '/views');
 
-console.log('Finished executing mongoose code')
-*/
+// Locate the assets
+app.use(express.static(__dirname + '/assets'));
 
-/*
- * Config for Production and Development
- */
-if (process.env.NODE_ENV === 'production') {
-    // Set the default layout and locate layouts and partials
-    app.engine('handlebars', exphbs({
-        defaultLayout: 'main',
-        layoutsDir: 'dist/views/layouts/',
-        partialsDir: 'dist/views/partials/'
-    }));
-
-    // Locate the views
-    app.set('views', __dirname + '/dist/views');
-    
-    // Locate the assets
-    app.use(express.static(__dirname + '/dist/assets'));
-
-} else {
-    app.engine('handlebars', exphbs({
-        // Default Layout and locate layouts and partials
-        defaultLayout: 'main',
-        layoutsDir: 'views/layouts/',
-        partialsDir: 'views/partials/'
-    }));
-
-    // Locate the views
-    app.set('views', __dirname + '/views');
-    
-    // Locate the assets
-    app.use(express.static(__dirname + '/assets'));
-}
 
 // Set Handlebars
 app.set('view engine', 'handlebars');
 
-
-
-/*
- * Routes
- */
-// Index Page
-//app.get('/', function(request, response, next) {
-//    response.render('index');
-//});
 
 
 keystone.set('mongo', 'mongodb://localhost:27017/keystone4');
@@ -125,10 +76,3 @@ app.get('/', function(req, res, next) {
 });
 keystone.app = app;
 keystone.start();
-
-
-/*
- * Start it up
- */
-//app.listen(process.env.PORT || port);
-console.log('Express started on port ' + port);
